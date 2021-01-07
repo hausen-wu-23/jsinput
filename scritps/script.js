78e5a86541a643899f14dd3af8e941cfdf2565f6
Hausen <hausenwu6sw@gmail.com>
2021-01-07 11:08:55 -0800

    Fixed

--- scritps/script.js
+++ scritps/script.js
@@ -4,10 +4,17 @@ const main = document.getElementById("bg");
 console.log(myDiv);
 
 function myClick () {
-  var name = document.getElementById('name').value;
-  var color = document.getElementById('color').value;
+  let name = document.getElementById('name').value;
+  let color = document.getElementById('color').value;
+  let checked = document.getElementById('check').checked;
   console.log(name, color);
   myDiv.innerHTML = "\n";
   myDiv.innerHTML += "\t\t<h1><mark>Hello, " + name + ", enjoy your colour!</mark></h1>\n";
   main.style.backgroundColor = color;
+  if (checked){
+    myDiv.innerHTML += "\t\t<h1><mark>checked</mark></h1>\n";
+  }
+  else{
+    myDiv.innerHTML += "\t\t<h1><mark>Not checked</mark></h1>\n";
+  }
 }
