9f3cf3e139e0d66217f1bdd013f79c021b7896e2
Hausen <hausenwu6sw@gmail.com>
2021-01-26 13:52:59 -0800

    Update script.js

--- scritps/script.js
+++ scritps/script.js
@@ -1,8 +1,8 @@
-const myDiv = document.getElementById('myDiv');
+const main_div = document.getElementById('myDiv');
 const main = document.getElementById('bg');
 
 
-var sessions = localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data")) : null;
+var sessions = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : null;
 
 if (sessions) {
   sessions.forEach((element, index) => {
@@ -12,13 +12,13 @@ if (sessions) {
       'checked': element.checked,
       'pizza': element.pizza,
     };
-    myDiv.innerHTML += '\t\t<h1>Session ' + (index + 1) + ': </h1> \n';
+    main_div.innerHTML += '\t\t<h1>Session ' + (index + 1) + ': </h1> \n';
     let user_message = '';
     user_message += '\t\t<h2>' + ((user_data.user_name) ? ('Last time you said your name was ' + user_data.user_name) : 'You didn\'t tell us your name');
     user_message += '. Your favourite colour is ' + user_data.color;
     user_message += user_data.checked ? '. You agreed our tos. ' : '. You did not agree our tos. ' 
     user_message += user_data.pizza ? 'You liked pizza.' : 'You are a monster!</h2>\n';
-    myDiv.innerHTML += user_message;
+    main_div.innerHTML += user_message;
   });
 }
 
@@ -35,23 +35,23 @@ function myClick() {
   user_data.pizza = document.getElementById('choose1').checked;
 
 
-  myDiv.innerHTML = '\n';
+  main_div.innerHTML = '\n';
 
   if (user_data.user_name) {
-    myDiv.innerHTML += '\t\t<h1><mark>Hello, ' + user_data.user_name + ', enjoy your colour!</mark></h1>\n';
+    main_div.innerHTML += '\t\t<h1><mark>Hello, ' + user_data.user_name + ', enjoy your colour!</mark></h1>\n';
   }
   else {
-    myDiv.innerHTML += '\t\t<h1><mark>You didnt enter your user_name, but still, enjoy your colour!</mark></h1>\n';
+    main_div.innerHTML += '\t\t<h1><mark>You didnt enter your user_name, but still, enjoy your colour!</mark></h1>\n';
   }
 
   main.style.backgroundColor = user_data.color;
 
-  myDiv.innerHTML += '\t\t<h1><mark>' + (user_data.checked ? 'Agreed to tos :D' : 'You disagreed to the tos :◖') + '</mark></h1>\n';
+  main_div.innerHTML += '\t\t<h1><mark>' + (user_data.checked ? 'Agreed to tos :D' : 'You disagreed to the tos :◖') + '</mark></h1>\n';
 
   if (user_data.pizza)
-    myDiv.innerHTML += '\t\t<h1><mark>You like pizza!</mark></h1>\n';
+    main_div.innerHTML += '\t\t<h1><mark>You like pizza!</mark></h1>\n';
   else
-    myDiv.innerHTML += '\t\t<h1><mark>You monster!</mark></h1>\n';
+    main_div.innerHTML += '\t\t<h1><mark>You monster!</mark></h1>\n';
   if (sessions)
     sessions.push(user_data);
   else
